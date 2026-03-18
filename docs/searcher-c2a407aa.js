bb9ec79d00cc3bdf47b21c9d2a9eb8068ce4df14
studentKim2000 <166790301+studentKim2000@users.noreply.github.com>
2026-03-18 21:51:03 +0900

    update

--- docs/searcher-c2a407aa.js
+++ docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-a6aaf15d.js',
+                path_to_root + 'searchindex-0816d57a.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');

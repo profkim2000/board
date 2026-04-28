7f02002a721c62f3b0b0801a470b88bc2f577f97
profkim2000 <profkimpro@gmail.com>
2026-04-28 10:26:01 +0900

    update

--- docs/searcher-c2a407aa.js
+++ docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-c1a37b86.js',
+                path_to_root + 'searchindex-5b0cc600.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
